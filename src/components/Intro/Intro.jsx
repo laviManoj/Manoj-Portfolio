3332383181b2f36f5bf79f0e341760ab524da003
laviManoj <lavigeremanoj@gmail.com>
2023-07-21 14:29:07 +0530

    Upadted the Portfolio

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
+import boy from "../../img/boy.jpeg";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
@@ -46,7 +46,7 @@ const Intro = () => {
       </div>
       {/* right image side */}
       <div className="i-right">
-        <img src={boy} alt="" height={600} padding="50px"/>
+        <img src={boy} alt="" height={500}  />
       
         {/* animation */}
         <motion.img
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,7 +6,7 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import Resume from './resume.pdf';
+import Resume from './Manoj H_Resume (1).pdf';
 
 const Services = () => {
   // context
